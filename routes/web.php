d41e1adbf0af4e6ebd331f391e534e9daeef9e8d
dengkangfa <dengkangfa@outlook.com>
2017-12-08 03:49:11 +0000

    Auto translate slug

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,9 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'edit', 'update']]);
 
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+// ? 意味着参数可选
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 

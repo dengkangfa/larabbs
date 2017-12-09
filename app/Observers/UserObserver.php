b086e898b1a77795277a0e8ca147c0ec3599e830
dengkangfa <dengkangfa@outlook.com>
2017-12-09 14:52:21 +0000

    Admin can manage roles and permissions

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -3,6 +3,8 @@
 namespace App\Observers;
 
 use App\Models\User;
+use App\Models\Topic;
+use App\Models\Reply;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -17,5 +19,11 @@ class UserObserver
     public function updating(User $user)
     {
         //
+    }
+
+    public function deleted(User $user)
+    {
+        Reply::where('user_id', $user->id)->delete();
+        Topic::where('user_id', $user->id)->delete();
     }
 }
---
+++ config/administrator/permissions.php
@@ -0,0 +1,58 @@
+<?php
+
+use Spatie\Permission\Models\Permission;
+
+return [
+    'title' => '权限',
+    'single' => '权限',
+    'model' => Permission::class,
+
+    'permission' => function () {
+        return Auth::user()->can('manage_users');
+    },
+
+    'action_permissions' => [
+        'create' => function ($model) {
+            return true;
+        },
+        'update' => function ($model) {
+            return true;
+        },
+        'delete' => function ($model) {
+            return false;
+        },
+        'view' => function ($model) {
+            return true;
+        }
+    ],
+
+    'columns' => [
+        'id' => [
+            'title' => 'ID'
+        ],
+        'name' => [
+            'title' => '标识'
+        ],
+        'operation' => [
+            'title' => '管理'
+        ],
+    ],
+
+    'edit_fields' => [
+        'name' => [
+            'title' => '标识（请慎重修改）',
+            'hint' => '修改权限标识会影响代码的调用，请不要轻易更改。'
+        ],
+        'roles' => [
+            'type' => 'relationship',
+            'tile' => '角色',
+            'name_field' => 'name'
+        ]
+    ],
+
+    'filters' => [
+        'name' => [
+            'title' => '标识'
+        ]
+    ]
+];

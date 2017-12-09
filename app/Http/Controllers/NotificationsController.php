03586dbc0bd99344339b8cd6177f5c5239b6004d
dengkangfa <dengkangfa@outlook.com>
2017-12-09 04:16:12 +0000

    Notify user in email with Queue supported

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -16,6 +16,7 @@ class NotificationsController extends Controller
     {
         // 获取登录用户的所有通知
         $notifications= Auth::user()->notifications()->paginate(20);
+        // 标记为已读，为读数量清零
         Auth::user()->markAsRead();
         return view('notifications.index', compact('notifications'));
     }
--- app/Notifications/TopicReplied.php
+++ app/Notifications/TopicReplied.php
@@ -9,7 +9,7 @@ use Illuminate\Notifications\Messages\MailMessage;
 
 use App\Models\Reply;
 
-class TopicReplied extends Notification
+class TopicReplied extends Notification implements ShouldQueue
 {
     use Queueable;
 
@@ -35,7 +35,7 @@ class TopicReplied extends Notification
     public function via($notifiable)
     {
         // 开启通知的频道
-        return ['database'];
+        return ['database', 'mail'];
     }
 
     public function toDatabase($notifiable)
@@ -54,5 +54,14 @@ class TopicReplied extends Notification
             'topic_id' => $topic->id,
             'topic_title' => $topic->title
         ];
+    }
+
+    public function toMail($notification)
+    {
+        $url = $this->reply->topic->link(['#reply' . $this->reply->id]);
+
+        return (new MailMessage)
+                ->line('你的话题有新回复！')
+                ->action('查看回复', $url);
     }
 }

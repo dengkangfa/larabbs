32a00639c4d7a6a710bf6d131f086d4453412c61
dengkangfa <dengkangfa@outlook.com>
2017-12-10 14:04:20 +0000

    User default avatar after signup

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -3,27 +3,15 @@
 namespace App\Observers;
 
 use App\Models\User;
-use App\Models\Topic;
-use App\Models\Reply;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
 class UserObserver
 {
-    public function creating(User $user)
-    {
-        //
-    }
-
-    public function updating(User $user)
-    {
-        //
-    }
-
-    public function deleted(User $user)
-    {
-        Reply::where('user_id', $user->id)->delete();
-        Topic::where('user_id', $user->id)->delete();
+    public function saving(User $user) {
+        if (empty($user->avatar)) {
+            $user->avatar = config('app.url') . '/default-avatar.png';
+        }
     }
 }

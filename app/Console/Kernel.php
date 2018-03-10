3a7456deb1855f69aec44a8cc83f4973193e3a2d
dengkangfa <dengkangfa@outlook.com>
2018-03-10 22:02:02 +0800

    generate-token command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,8 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
          Commands\CalculateActiveUser::class,
-         Commands\SyncUserActivedAt::class
+         Commands\SyncUserActivedAt::class,
+         Commands\GenerateToken::class
     ];
 
     /**

5daf8904660e512342dcae87e6e2f07f929be546
dengkangfa <dengkangfa@outlook.com>
2018-03-15 14:41:40 +0800

    topic update api

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api',
-    'middleware' => 'serializer:array'
+    'middleware' => ['serializer:array', 'bindings']
 ], function($api) {
     $api->group([
         'middleware' => 'api.throttle',
@@ -52,6 +52,8 @@ $api->version('v1', [
             // 发布话题
             $api->post('topics', 'TopicsController@store')
                 ->name('api.topics.store');
+            $api->patch('topics/{topic}', 'TopicsController@update')
+                ->name('api.topics.update');
         });
     });
 });

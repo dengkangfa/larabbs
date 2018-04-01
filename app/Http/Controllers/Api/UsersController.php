f26a1ab9540172ace3def3c0b30997de516b287f
dengkangfa <dengkangfa@outlook.com>
2018-04-01 23:53:18 +0800

    actived users

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -14,6 +14,11 @@ class UsersController extends Controller
         return $this->response->item($this->user(), new UserTransformer());
     }
 
+    public function activedIndex(User $user)
+    {
+        return $this->response->collection($user->getActiveUsers(),  new UserTransformer());
+    }
+
     public function store(UserRequest $request)
     {
         $verifyData = \Cache::get($request->verification_key);

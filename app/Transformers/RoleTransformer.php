344d148d92d7bd36acb77f81b6b5fb5bb8d6b30b
dengkangfa <dengkangfa@outlook.com>
2018-04-01 19:51:02 +0800

    include user roles

---
+++ app/Transformers/RoleTransformer.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Transformers;
+
+use League\Fractal\TransformerAbstract;
+
+class RoleTransformer extends TransformerAbstract
+{
+    public function transform(Role $role)
+    {
+        return [
+            'id' => $role->id,
+            'name' => $role->name
+        ];
+    }
+}

a08195a572484176ed882419086f49fa8de842b6
dengkangfa <dengkangfa@outlook.com>
2018-04-02 01:51:38 +0800

    locale

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -79,5 +79,7 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         // 访问节流，类似于 『1 分钟只能请求 10 次』的需求，一般在 API 中使用
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        // 接口语言设置
+        'change-locale' => \App\Http\Middleware\ChangeLocale::class
     ];
 }

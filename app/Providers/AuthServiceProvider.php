78bfc2c79d8f8060f50dc112544f982e7c945ccc
dengkangfa <dengkangfa@outlook.com>
2018-04-03 16:50:31 +0800

    add passport

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,9 +3,11 @@
 namespace App\Providers;
 
 use Auth;
+use Carbon\Carbon;
 use Horizon;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -28,6 +30,13 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+
+        // Passport 的路由
+        Passport::routes();
+        // access_token 过期时间
+        Passport::tokensExpireIn(Carbon::now()->addDays(15));
+        // refreshTokens 过期时间
+        Passport::refreshTokensExpireIn(Carbon::now()->addDays(30));
 
         Horizon::auth(function ($request) {
             // 是否是站长
